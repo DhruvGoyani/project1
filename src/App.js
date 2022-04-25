513d6244d1411efa1852d409b26ebbf8c6ea1981
DhruvGoyani <dhruvgoyani7@gmail.com>
2022-04-25 15:49:07 +0530

    dhrhtgd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Container/Home/Home';
+import Department from './Container/Department/Department';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <switch>
+      <Route exact path={"/"} component={Home} />
+      <Route exact path={"/Department"} component={Department} />
+    </switch>
+    <Footer />
+    </>
   );
 }
 
