b4d25481676ff0fc2c00e215790fe546b71719ba
DhruvGoyani <dhruvgoyani7@gmail.com>
2022-05-02 11:53:44 +0530

    pr1

--- src/Container/Auth/Auth.js
+++ src/Container/Auth/Auth.js
@@ -19,7 +19,7 @@ function Auth(props) {
                 {
                     password ?
                         <div className="section-title">
-                            <h2 className='centeerr'>reset password</h2>
+                            <h2 className='centeerr'>Forget password</h2>
                         </div>
                         : usertype === 'Login' ?
                             <div classname="section-title">
@@ -34,10 +34,17 @@ function Auth(props) {
                 <div action method="post" className="php-email-form">
                     <div className="row">
                         {
-                            password ? <div className="col-md-12 form-group mt-3 mt-md-0">
-                                <input type="email" className="form-control" name="name" id="name" placeholder="Your Name" data-rule="name" data-msg="Please enter a valid email" />
-                                <div className="validate" />
-                            </div> : usertype === 'Login' ?
+                            password === true ? <div className="col-md-12 form-group mt-3 mt-md-0">
+                            <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                            <div className="validate" />
+                        </div> : null 
+                        }
+                        {
+                            // password === true ? <div className="col-md-12 form-group mt-3 mt-md-0">
+                            //     <input type="name" className="form-control" name="name" id="name" placeholder="Your Name" data-rule="name" data-msg="Please enter a valid email" />
+                            //     <div className="validate" />
+                            // </div> : 
+                            usertype === 'Login' ?
                                 null
                                 :
                                 <div className="col-md-12 form-group mt-3 mt-md-0">
@@ -55,32 +62,38 @@ function Auth(props) {
                             <div className="validate" />
                         </div>
                     </div>
-
-
                     {
+                        password ? 
+                        <div className="text-center">
+                            <button type="submit" onClick={() => passwordchng()}>Forget password</button><br></br>
+                        </div>  
+                        :
                         usertype === 'Login' ?
                             <div className="text-center">
                                 <button type="submit" onClick={() => handleLogin()}>Login</button><br></br>
                             </div> :
                             <div className="text-center">
                                 <button onClick={() => handleSignup()} type={'submit'}>signup</button>
-                            </div>
+                            </div> 
                     }
                     {
-                        password ? <div className='text-center'><button onClick={() => setpassword('password')}>Forget password</button></div> :
+                        
+                            password === true ?
+                            <div className='text-center mt-5'>
+                            <span>already have an account ?</span>
+                            <button onClick={() => setpassword(false)}>Login</button>
+                        </div> :
                         usertype === 'Login' ?
                             <div className='text-center mt-5'>
                                 <span>create a New account</span>
                                 <button onClick={() => { setuserType('Signup') }} >signup</button> <br></br>
+                                <button onClick={() => {setpassword(true)}}>Forget password</button>
                             </div> :
                             <div className='text-center mt-5'>
                                 <span>already have an account ?</span>
                                 <button onClick={() => { setuserType('Login') }} >Login</button>
                             </div>
                     }
-                    <div>
-
-                    </div>
                 </div>
             </div>
         </section>
